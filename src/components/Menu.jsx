aaef858272f3e0a7b4abab2e42a429ccba702f11
Devonte MacGlashan <devontem@yahoo-inc.com>
2017-12-23 09:13:03 -0800

    updated the menu to have relevant files

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,6 +5,8 @@ import ActionGrade from 'material-ui/svg-icons/action/grade';
 import ContentSend from 'material-ui/svg-icons/content/send';
 import ContentDrafts from 'material-ui/svg-icons/content/drafts';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentAddCircle from 'material-ui/svg-icons/content/add-circle';
+import ActionNoteAdd from 'material-ui/svg-icons/action/note-add';
 import CommunicationForum from 'material-ui/svg-icons/communication/forum';
 import ActionSearch from 'material-ui/svg-icons/action/search';
 import ActionHome from 'material-ui/svg-icons/action/home';
@@ -14,6 +16,7 @@ import ActionStore from 'material-ui/svg-icons/action/store';
 import ActionViewModule from 'material-ui/svg-icons/action/view-module';
 import Divider from 'material-ui/Divider';
 
+
 import ActionInfo from 'material-ui/svg-icons/action/info';
 import Card from 'material-ui/Card';
 
@@ -22,27 +25,28 @@ const Menu = (props) => {
 		<Card>
 			<List>
 		      <ListItem primaryText="Home" leftIcon={<ActionHome />} />
-		      <ListItem primaryText="Forum" leftIcon={<CommunicationForum />} />
-		      <ListItem primaryText="Search" leftIcon={<ActionSearch />} />
-		      <ListItem primaryText="Tags" leftIcon={<ActionViewModule />} />
-		      <ListItem primaryText="Countries" leftIcon={<SocialPublic />} />
-		      <ListItem primaryText="Venues" leftIcon={<ActionStore />} />
-		      <ListItem primaryText="Social" leftIcon={<SocialPeople />} />
+		      <ListItem primaryText="Find Jams" 
+		      			leftIcon={<ActionStore />} 
+		      			nestedItems={[
+		      					<ListItem primaryText="Search" href="/search" leftIcon={<ActionSearch />} />,
+		      					<ListItem primaryText="Tags" leftIcon={<ActionViewModule />} />,
+		      					<ListItem primaryText="Countries" leftIcon={<SocialPublic />} />,
+		      				]}/>
+		      <ListItem primaryText="Forum" href="/forum" leftIcon={<CommunicationForum />} />
 		      <Divider />
-		      <ListItem primaryText="Add Jam" leftIcon={<ContentAdd />} />
+		      <ListItem primaryText="Add Jam" leftIcon={<ContentAddCircle />} />
+		      <ListItem primaryText="Add Review" leftIcon={<ActionNoteAdd />} />
 		    </List>
 
 		    { (props.loggedIn) ?
 		    	<div>
 			    	<Divider />
 			    	<List>
-				      <ListItem primaryText="Home" leftIcon={<ContentInbox />} />
-				      <ListItem primaryText="Forum" leftIcon={<ActionGrade />} />
-				      <ListItem primaryText="Search" leftIcon={<ContentSend />} />
-				      <ListItem primaryText="Tags" leftIcon={<ContentSend />} />
-				      <ListItem primaryText="Countries" leftIcon={<ContentDrafts />} />
-				      <ListItem primaryText="Venues" leftIcon={<ContentInbox />} />
-				      <ListItem primaryText="Social" leftIcon={<ContentInbox />} />
+				      <ListItem primaryText="My Profile" leftIcon={<ContentInbox />} />
+				      <ListItem primaryText="My Venues" leftIcon={<ActionGrade />} />
+				      <ListItem primaryText="Following" leftIcon={<ContentSend />} />
+				      <ListItem primaryText="Followers" leftIcon={<ContentSend />} />
+				      <ListItem primaryText="My Reviews" leftIcon={<ContentDrafts />} />
 				      <Divider />
 				      <ListItem primaryText="Add Jam" leftIcon={<ContentInbox />} />
 				    </List>
