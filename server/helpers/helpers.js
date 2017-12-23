4a6d65ca784bf17679eec36d338ba4f1e4a12e12
Devonte MacGlashan <devontem@yahoo-inc.com>
2017-12-23 09:14:25 -0800

    added image upload functionality

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,4 +1,13 @@
 var bcrypt = require('bcrypt');
+var fs = require('fs');
+var path = require('path');
+var MIME_TYPES = {
+	'image/gif': '.gif', 
+	'image/png': '.png', 
+	'image/jpeg': '.jpeg', 
+	'image/bmp': '.bpmp', 
+	'image/webp': '.webp'
+};
 
 module.exports.hashPassword = function(password){
 	var salt = bcrypt.genSaltSync(10);
@@ -11,4 +20,46 @@ module.exports.checkPassword = function(password, database_hash){
 	var correct_password = bcrypt.compareSync(password, database_hash);
 
 	return correct_password;
+}
+
+module.exports.uploadImages = function(req){
+
+	return new Promise(function(resolve, reject){
+		console.log('req.files');
+		if (!req.files){
+			console.log('failing in req.files')
+	        reject(null);
+	    }
+
+	    // The name of the input field for the file
+	    var file = req.files.photo;
+	    var file_name = getHash() + getFileType(file.mimetype);
+	    var destination_path = '/image_uploads/' + file_name;
+
+	    // Use the mv() method to place the file somewhere on your server
+	    file.mv('./build' + destination_path, function(err) {
+	        if (err)
+	          reject(null);
+
+	        resolve(destination_path);
+	    });
+	});
+
+}
+
+function getFileType(item){
+	if (MIME_TYPES[item]){
+		return MIME_TYPES[item];
+	}
+	return '';
+}
+
+function getHash() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 8; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
 }
