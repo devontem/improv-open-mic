514546e0591277aebdc7c164456ac95e279a5f4b
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-01-05 12:52:45 -0800

    added public directory back

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -31,6 +31,7 @@ module.exports = function(app, express){
 	app.use(fileUpload());
 
 	// serving static files
+	app.use(express.static(path.resolve(__dirname, '..', '..' , 'public')));
 	app.use(express.static(path.resolve(__dirname, '..', '..' , 'build')));
 
 	// colored/detailed server logs
